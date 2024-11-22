e2c531bc98705feb5abfe94601fc72b51ddfdc07
Inigo2022 <125950208+Inigo2022@users.noreply.github.com>
2024-11-23 02:10:58 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the largest country in the world by land area?",
+        answer: "C. Russia",
+        options: [
+            "A. India",
+            "B. Canada",
+            "C. Russia",
+            "D. USA"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the medical term for high blood pressure?",
+        answer: "B. Hypertension",
+        options: [
+            "A. Hypotension",
+            "B. Hypertension",
+            "C. Hyperglycemia",
+            "D. Acidosis"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many bones are there in the adult human body?",
+        answer: "B. 206",
+        options: [
+            "A. 186",
+            "B. 206", 
+            "C. 226",
+            "D. 246"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which country has the most islands in the world?",
+        answer: "D. Sweden",
+        options: [
+            "A. Philippines",
+            "B. Indonesia",
+            "C. Canada",
+            "D. Sweden"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What parent company owns both Instagram and WhatsApp?",
+        answer: "C. Meta",
+        options: [
+            "A. Google",
+            "B. Apple",
+            "C. Meta", 
+            "D. Amazon"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the main function of red blood cells?",
+        answer: "A. Carrying oxygen",
+        options: [
+            "A. Carrying oxygen",
+            "B. Fighting infections",
+            "C. Clotting blood", 
+            "D. Regulating body temperature"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who acted as Iron Man in the Marvel Franchise?",
+        answer: "A. Robert Downey Jr.",
+        options: [
+            "A. Robert Downey Jr.",
+            "B. Chris Hemsworth",
+            "C. Chris Evans", 
+            "D. Tom Holland"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which country is known as the Land of the Rising Sun?",
+        answer: "C. Japan",
+        options: [
+            "A. China",   
+            "B. South Korea",
+            "C. Japan", 
+            "D. Thailand"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is cognitive dissonance?",
+        answer: "A. Conflict between attitudes and behaviors",
+        options: [
+            "A. Conflict between attitudes and behaviors", 
+            "B. Increased social interaction",
+            "C. Heightened emotional states", 
+            "D. Enhanced cognitive function"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the average lifespan of a domestic cat?",
+        answer: "B. 10-15 years",
+        options: [
+            "A. 5-8 years",   
+            "B. 10-15 years",
+            "C. 15-20 years", 
+            "D. 20-25 years" 
+        ]
+    },
+];
